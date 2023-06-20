53b6e4be57fcdb649c49d1f4b572d2b3694e7f1e
Christina <iss21041@students.del.ac.id>
2023-06-20 15:27:39 +0700

    adding combobox

--- index.php
+++ index.php
@@ -5,14 +5,20 @@ require "Database.php";
 
 require "router.php";
 
-
-
-
 //connect to database
 
 // $config = require "config.php";
 
 // $db = new Database($config['database']);
+?>
+<!-- 
+// $result = $db->connect("SELECT NIM FROM mahasiswa")->fetchAll(PDO::FETCH_ASSOC);
+
+// foreach ($result as $row) {
+//   echo "<li>" . $row["NIM"] . "</li>";
+// }
+
+
 // $result = $db->connect("SELECT * FROM daftarkontak")->fetchAll(PDO::FETCH_ASSOC);
 
 // foreach ($result as $row) {
@@ -41,4 +47,4 @@ require "router.php";
 // $person->name = "John Wick";
 // $person->age = 30;
 
-// $person->breathe();
+// $person->breathe(); -->
--- views/index.view.php
+++ views/index.view.php
@@ -4,32 +4,17 @@
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
 
-  <!-- Pilih NIM Mahasiswa -->
-    <form method="POST" action="proses.php">
-      <div class="form-floating">
-        <select class="form-select" id="floatingSelect" name="menu" aria-label="Floating label select example" onchange="this.form.submit()">
-          <option selected>...</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-        <label for="floatingSelect">NIM Mahasiswa: </label>
-      </div>
-    </form>
+    <h1>Form Input dengan Dropdown</h1>
+    <br>
+    <div class="dropdown input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">NIM Anda:</span>
+      <input type="text" id="inputField" class="form-control" onkeyup="filterOptions()" placeholder="NIM Anda: " aria-label="NIM Anda: " aria-describedby="basic-addon1">
+    </div>
+    <div id="dropdownMenu" class="dropdown-item dropdown-menu"></div>
+
+    
 
 
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-      <label class="form-check-label" for="flexCheckDefault">
-        Default checkbox
-      </label>
-    </div>
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-      <label class="form-check-label" for="flexCheckChecked">
-        Checked checkbox
-      </label>
-    </div>
   </div>
 </main>
 
